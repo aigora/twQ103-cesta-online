c4f27e3ea2331fd2e41900d0b6bdac818e41b204
Diego54632 <47477240+Diego54632@users.noreply.github.com>
2019-04-28 21:03:06 +0200

    Update Menu.c

--- Menu.c
+++ Menu.c
@@ -1,48 +1,99 @@
-/* 
-	Al querer comprar un producto, pedir que inicie sesion o registrarse si aun no lo ha hecho.
-	En finalizar comprar recordar pedir si se tiene algun codigo de descuento.
-*/
+/*CESTA ONLINE*/
+
+//En finalizar comprar recordar pedir si se tiene algun codigo de descuento.
+
 
 #include <stdio.h>
 #define NUM_MAX_USUARIOS 100
 
-struct persona{
-	char nombre[20];
+struct persona
+{
+	char nombre[50];
 	int clave;
 }usuarios[NUM_MAX_USUARIOS];
 	
 
 int main()
 {
+	
 	int opcion;
+	int nusuarios = 0, i, x=0;
+	char inicio_nombre[50];
+	int inicio_clave;
 	
 
-	int nusuarios = 0;
-	
-	printf("Menu en estado beta\n\n\n");
-	
 	
 	do
 	{
-		printf("\nMENU DE OPCIONES\n1-Registrarse\n2-Iniciar sesion\n3-Salir de la pagina");
+		//SE LEE EL FICHERO
+		FILE *fichero;		
+		fichero = fopen("datos.txt", "r");			
+		if(fichero==NULL)
+		{
+			printf("No es posible abrir el fichero.\n");
+		}
+		while (fscanf(fichero, "%s%i", usuarios[nusuarios].nombre, &usuarios[nusuarios].clave)!=EOF)
+		{
+			nusuarios++;
+		}
+		fclose(fichero);
+		
+		//................................................................//
+				
+		printf("\nMENU DE OPCIONES\n1-Registrarse\n2-Iniciar sesion\n3-Salir de la pagina\n");
 		scanf("%i", &opcion);
 		switch(opcion)
 		{
 			case 1:
-				printf("introduce el usuario\n");
+				fichero=fopen("datos.txt", "a");
+				
+				printf("\n\nintroduce el nombre de usuario\n");
 				fflush(stdin);
-	            gets(persona1.nombre);
+	            gets(usuarios[nusuarios].nombre);
 	            
 				printf("introduce la clave\n");
-				scanf("%i",&persona1.clave);
+				scanf("%i",&usuarios[nusuarios].clave);
 				
-				printf("\n su usuario es: %s",persona1.usuario);
-				printf("\n su clave es: %i",persona1.clave);
-					
-			
+				
+				fprintf(fichero,"%s ", usuarios[nusuarios].nombre);
+				fprintf(fichero,"%i\n", usuarios[nusuarios].clave);
+				
+				fclose(fichero);
+								
 			break;
 			
 			case 2:
+				printf("\n\nintroduce el nombre de usuario\n");
+				fflush(stdin);
+	            gets(inicio_nombre);
+	            
+				printf("introduce la clave\n");
+				scanf("%i",&inicio_clave);
+				
+				for(i=0;i<nusuarios;i++)
+				{
+					/*printf("\n\n%s\n", inicio_nombre);
+					printf("%s\n\n\n", usuarios[i].nombre);
+					printf("%i\n", inicio_clave);
+					printf("%i\n", usuarios[i].clave);*/
+					
+					if(0==strcmp(inicio_nombre,usuarios[i].nombre)&&inicio_clave==usuarios[i].clave)
+					{
+						//printf("okeeeyy\n");
+						x=1;
+						break;
+					}
+					
+				}
+				if(x!=0)//x==1
+				{
+					printf("Puede inciar sesion\n");
+				}
+				else//x=0
+				{
+					printf("Error al inciar sesion\n");
+				}
+				
 			break;
 			
 			
@@ -59,21 +110,6 @@ int main()
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	return 0;
 	
 }
-
